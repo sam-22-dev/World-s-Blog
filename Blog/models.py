0c38fce0a80eb20bc26b441522df4460582226b9
Sameh Ahmed <sameh.elb.22@gmail.com>
2025-10-01 09:45:57 +0300

    add Answers to models

--- Blog/models.py
+++ Blog/models.py
@@ -6,8 +6,8 @@ from taggit.managers import TaggableManager
 
 class Question (models.Model):
     author = models.ForeignKey(User,on_delete=models.SET_NULL,null=True,blank=True,related_name='Question_User')
-    question = models.CharField(max_length=3000)
-    content = models.TextField(max_length=30000)
+    question = models.CharField(max_length=300)
+    content = models.TextField(max_length=3000)
     created_date = models.DateTimeField(default=timezone.now)
     draft = models.BooleanField(default=True)
     tags = TaggableManager()
@@ -16,3 +16,12 @@ class Question (models.Model):
         return self.question
 
 
+
+class Answer (models.Model):
+    author = models.ForeignKey(User,on_delete=models.CASCADE,related_name='answer_author')
+    question = models.ForeignKey(Question,on_delete=models.CASCADE,related_name='answer_question')
+    answer = models.TextField(max_length=5000)
+    created_date = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return str(self.author)
