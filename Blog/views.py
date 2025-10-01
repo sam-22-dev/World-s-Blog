cf871896118da3e2e62e13c8620c92e8a11fc756
Sameh Ahmed <sameh.elb.22@gmail.com>
2025-10-01 11:14:00 +0300

    add Answers to views

--- Blog/views.py
+++ Blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from .models import Question
+from .models import Question,Answer
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from .forms import AnswerForm
 
 
 class QuestionList(ListView):
@@ -9,13 +10,23 @@ class QuestionList(ListView):
 
 def question_details(request,question_id):
     data = Question.objects.get(id = question_id)
-    return render(request,'Blog/question_details.html',{'question':data})
+    question_answers = Answer.objects.filter(question=data)
+    if request.method == 'POST':
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            myform = form.save(commit=False)
+            myform.author = request.user
+            myform.question = data
+            myform.save()
+    else:
+        form = AnswerForm()
+    return render(request,'Blog/question_details.html',{'question':data, 'answers':question_answers, 'form':form})
 
 
 class AddQuestion(CreateView):
     model = Question
     fields = ['author','question','content','created_date','draft','tags']
-    success_url = ('/question')
+    success_url = ('/questions')
 
 
 
